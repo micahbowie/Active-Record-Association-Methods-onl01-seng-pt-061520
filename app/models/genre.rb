915575d2d246628da0c9d0ff3213df4f63a9d91c
Micah Bowie <micahbowie20@gmail.com>
2020-09-06 20:46:12 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,14 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    Self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    Self.artists.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+   Self.artists.name
   end
 end
